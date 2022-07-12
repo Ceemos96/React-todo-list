342bdc5438dae49728d2c2c71dc1905d67aa81d7
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-12 14:07:21 -0700

    Create component files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello from Create React App</h1>;
+import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
